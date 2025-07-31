02abfcee6c613c4dbe39206201665d22d9b137d4
HaswinAI <haswinraj.aiml@gmail.com>
2025-07-31 23:54:20 +0530

    DSA SOLVE DAILY: valid Anagram today

--- string/anagram.py
+++ string/anagram.py
@@ -1,10 +1,8 @@
+from collections import Counter
+
 def is_anagram(s,t):
-    return counter(s) == counter(t)
+    return Counter(s) == Counter(t)
 
 s = "listen"
 t = "silent"
 print(is_anagram(s,t))
-if is_anagram(s,t):
-    print(True)
-else:
-    print(False)
