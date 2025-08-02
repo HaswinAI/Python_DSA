116793944abfdf99ef207e1d5b21d862a2a27775
HaswinAI <haswinraj.aiml@gmail.com>
2025-08-03 00:22:11 +0530

    DAILY SOLVE DSA: Bubble sorting

--- search_sort/Sorting.py
+++ search_sort/Sorting.py
@@ -10,11 +10,10 @@ def selection_sort(arr):
 
 def bubble_sort(arr):
     n = len(arr)
-    for i in range(n-1):
-        for j in range(i,n-1):
-            if arr[i] < arr[j]:
-                j = i
-        arr[i], arr[j] = arr[j], arr[i]
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
         
 arr = [13,46,24,52,20,9]
