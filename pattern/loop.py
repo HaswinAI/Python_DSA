b711faeb304d0353dfce19259950357136e5ea23
HaswinAI <haswinraj.aiml@gmail.com>
2025-08-06 23:55:29 +0530

    SOLVE SMALL LOOPS

--- pattern/loop.py
+++ pattern/loop.py
@@ -4,3 +4,13 @@ for i in str1:
     print(str1[1:index+1])
     index+=1"""
 
+a = int(input("Enter the num A:"))
+b = int(input("Enter the num B:"))
+c = int(input("Enter the num C:"))
+if a > b and a > c:
+    print("A is greater") 
+elif b > a and b > c:
+    print("B is greater")
+else:
+    print("C is greater")
+    
