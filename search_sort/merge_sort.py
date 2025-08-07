689b8de13fc0bcef359b7314f42cb7e5bedfce60
HaswinAI <haswinraj.aiml@gmail.com>
2025-08-08 01:01:58 +0530

    Merge sort today

--- search_sort/merge_sort.py
+++ search_sort/merge_sort.py
@@ -1,34 +1,36 @@
 def merge_sort(arr):
+    #Base Case
     if len(arr) <= 1:
         return arr
-
-    mid = len(arr) // 2
     
+    
+    mid  = len(arr) // 2
     left_half = merge_sort(arr[:mid])
     right_half = merge_sort(arr[mid:])
-    
+
     return merge(left_half,right_half)
 
-def merge(left,right):
-    sorted_list = []
+def merge(left, right):
+    result = []
     i = j = 0
-
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
-            sorted_list.append(left[i])
+            result.append(left[i])
             i += 1
         else:
-            sorted_list.append(right[j])
+            result.append(right[j])
             j += 1
-
+    
     while i < len(left):
-        sorted_list.append(left[i])
+        result.append(left[i])
         i += 1
+
     while j < len(right):
-        sorted_list.append(right[j])
+        result.append(right[j])
         j += 1
-    return sorted_list
+
+    return result
 
 
-arr = [3,7,1,4,8,6]
+arr = [3,7,1,4,8,2,6]
 print(merge_sort(arr))
