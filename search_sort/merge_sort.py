8a4b797a6ebeec5e590990e5119ec33286c4b925
HaswinAI <haswinraj.aiml@gmail.com>
2025-08-07 23:58:00 +0530

    DSA daily solve: merge sort

--- search_sort/merge_sort.py
+++ search_sort/merge_sort.py
@@ -1,2 +1,34 @@
 def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
     
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+    
+    return merge(left_half,right_half)
+
+def merge(left,right):
+    sorted_list = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
+
+    while i < len(left):
+        sorted_list.append(left[i])
+        i += 1
+    while j < len(right):
+        sorted_list.append(right[j])
+        j += 1
+    return sorted_list
+
+
+arr = [3,7,1,4,8,6]
+print(merge_sort(arr))
