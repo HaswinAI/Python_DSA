d01db6650e3346f83707b9aee1f9c141baa45851
HaswinAI <haswinraj.aiml@gmail.com>
2025-08-09 12:52:20 +0530

    DSA recursive bubble sort

---
+++ search_sort/bubble_sort.py
@@ -0,0 +1,39 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
+
+def recursive_bsort(arr, n=None):
+    if n is None:
+        n = len(arr)
+    
+    if n == 1:
+        return
+    
+    for i in range(n - 1):
+        if arr[i] > arr[i+1]:
+            arr[i],arr[i+1] = arr[i+1], arr[i]
+    
+    recursive_bsort(arr, n - 1)
+
+arr = [9,7,3,4,2,3,1]
+def recursive_bsort(arr, n=None):
+    if n is None:
+        n = len(arr)
+    
+    if n == 1:
+        return
+    
+    for i in range(n - 1):
+        if arr[i] > arr[i+1]:
+            arr[i],arr[i+1] = arr[i+1], arr[i]
+    
+    recursive_bsort(arr, n - 1)
+
+arr = [9,7,3,4,2,3,1]
+print(bubble_sort(arr))
+recursive_bsort(arr)
+print(arr)
