00c249ca82707defc1891fdca4cffbe138520106
HaswinAI <haswinraj.aiml@gmail.com>
2025-08-10 23:55:10 +0530

    DSA solve daily: quick sort - last and first ement as pivot

--- search_sort/quick_sort.py
+++ search_sort/quick_sort.py
@@ -5,6 +5,17 @@ def quick_sort(arr,low,high):
         quick_sort(arr,pivot_index+1,high)
 
 def partition(arr,low,high):
+    #if choose the last element as pivot
+    pivot_index = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] <= pivot_index:
+            i += 1
+            arr[i],arr[j] = arr[j], arr[i]
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return i+1
+
+    """
     pivot_index = arr[low]
     # if I using first index then I should use two pointer
     left = low + 1
@@ -19,8 +30,9 @@ def partition(arr,low,high):
 
         arr[left],arr[right] = arr[right],arr[left]
     arr[low],arr[right] = arr[right], arr[low]
-    
+
     return right
+    """
 
 arr = [8,7,6,5,4,3,2,1]
 quick_sort(arr,0,len(arr)-1)
