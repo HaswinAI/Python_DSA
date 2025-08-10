cb34762bce99d2e0139dd3a2da2eca9bca1e0928
HaswinAI <haswinraj.aiml@gmail.com>
2025-08-10 23:13:46 +0530

    QUICK sort

--- search_sort/quick_sort.py
+++ search_sort/quick_sort.py
@@ -1,27 +1,28 @@
 def quick_sort(arr,low,high):
-    if low < high:        
-        pivot = partition(arr,low,high)
-        quick_sort(arr,low,pivot-1)
-        quick_sort(arr,pivot+1,high)
+    if low < high:
+        pivot_index = partition(arr,low,high)
+        quick_sort(arr,low,pivot_index-1)
+        quick_sort(arr,pivot_index+1,high)
 
 def partition(arr,low,high):
-    #if using low indx need two pointer
-    pivot = arr[low]
+    pivot_index = arr[low]
+    # if I using first index then I should use two pointer
     left = low + 1
     right = high
     while True:
-            
-        while left <= right and arr[left] <= pivot:
+        while left <= right and arr[left] <= pivot_index:
             left += 1
-        while left <= right and arr[right] >= pivot:
+        while left <= right and arr[right] >= pivot_index:
             right -= 1
         if left > right:
             break
-        
+
         arr[left],arr[right] = arr[right],arr[left]
-    arr[low],arr[right] = arr[right],arr[low]
+    arr[low],arr[right] = arr[right], arr[low]
+
+
     return right
 
-arr = [4,6,1,2,3,8,9,1]
+arr = [8,7,6,5,4,3,2,1]
 quick_sort(arr,0,len(arr)-1)
 print(arr)
