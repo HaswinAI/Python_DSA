7ba0745b1f68f0eb5eaafdb581b029d3bc0b263c
HaswinAI <haswinraj.aiml@gmail.com>
2025-08-28 23:34:37 +0530

    DSA SOLVE : SLiding window

--- array/max_sub_array.py
+++ array/max_sub_array.py
@@ -11,3 +11,13 @@ nums = [-2,1,-3,4,-1,2,1,-5,4]
 print(max_sub_array(nums))
 """
 #sliding window
+def sliding_window(arr):
+    k = 3
+    window_sum = sum(arr[:k])
+    max_sum = window_sum
+    for i in range(k,len(arr)):
+        window_sum += arr[i] - arr[i-k]
+        max_sum =  max(window_sum,max_sum)
+    return max_sum
+arr = [2,1,5,1,3,-9,2]
+print(sliding_window(arr))
