346a065720c9f3edef37f8cfd6c2a3f316deabff
HaswinAI <haswinraj.aiml@gmail.com>
2025-08-29 23:42:44 +0530

    DSA SOlvE Daily

---
+++ array/1343.py
@@ -0,0 +1,16 @@
+#number of subarray of size K and avg >= thresehold
+def subarray(arr,k,thresehold):
+    res = 0
+    window_sum = sum(arr[:k])
+    for i in range(k,len(arr)):
+        window_sum += arr[i] - arr[i-k]
+        if window_sum >= thresehold:
+            res += 1
+        window_sum -= arr[i]
+
+    return res
+
+arr = [2,2,2,2,5,5,5,8]
+k = 3
+thresehold = 4
+print(subarray(arr,k,thresehold))
